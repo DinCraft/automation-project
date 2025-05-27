89d2f18689625a60dcb683a673132b0888b19bcc
DinCraft <idiniyar2006@mail.ru>
2025-05-27 07:44:44 +0300

    save

--- python/main.py
+++ python/main.py
@@ -1,4 +1,8 @@
+import os
+import sqlite3
+
 from dincraft.common.table import Table
+from table_command import TableCommand
 
 print("Table maker")
 
@@ -9,6 +13,18 @@ while True:
     args = command.split()
     if args[0] == "open":
         if len(args) != 2:
-            print("Error! One argument required: open <file.db>")
-    if args[0] == "create":
-    
+            print("Error! One argument required: open <pathToFile.db>")
+        if not os.path.exists(args[1]):
+            print("Error! File doesn't exist!")
+            continue
+        connection = sqlite3.connect(args[1])
+    elif args[0] == "create":
+        if len(args) != 2:
+            print("Error! One argument required: create <pathToFile.db>")
+            continue
+        if os.path.exists(args[1]):
+            print("Error! File already exists!")
+            continue
+        connection = sqlite3.connect(args[1])
+    else:
+        print("Command not found!")
